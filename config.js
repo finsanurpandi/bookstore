db93216cac49344df1d9db101eba2f573a33a82c
finsanurpandi <finsaworks@gmail.com>
2018-01-09 15:34:41 +0700

    trying...2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "database" : "mongodb://localhost:27017/bookstore",
+  "database" : "mongodb://root:abc123@ds011389.mlab.com:11389/cobanode",
   "port" : process.env.PORT || 3000,
   "secretKey" : "helloWorld"
 }
